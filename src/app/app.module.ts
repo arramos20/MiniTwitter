144366876ebbe6347c9d4a276caf67e90fed0b19
Alejandro Ramírez Ramos <ramirez.raale20@aulas.triana>
2020-11-24 10:40:20 +0100

    v1.1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { RegistroUsuarioComponent } from './registro-usuario/registro-usuario.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegistroUsuarioComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/registro-usuario/registro-usuario.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-registro-usuario',
+  templateUrl: './registro-usuario.component.html',
+  styleUrls: ['./registro-usuario.component.scss']
+})
+export class RegistroUsuarioComponent implements OnInit {
+
+  constructor() { }
+    
+  ngOnInit(): void {
+  }
+
+/*
+{
+"username": "profesor",
+"email": "dam2019@gmail.com",
+"password": "1234",
+"code": "UDEMYANDROID"
+}
+*/
+}
